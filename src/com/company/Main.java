c48d2057755ec071afce271c5cec1966316f0abc
Kurmanjan <kumarska88@gmail.com>
2019-08-26 23:07:25 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Asan";
+        int age = 23;
+        int temp = 28;
+
+
+        if (temp>30 ||temp<=20 &&age>=20 || age<=45){
+            System.out.println(name+age+"progulka");
+        }
+        else {
+            System.out.println("segodnya dojd");
+        }
+    }
+}
